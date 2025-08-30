87555e9711f1be14b88a13303f4b7198e954a8a5
Nicholas Weist <weist.nick@outlook.com>
2025-08-29 20:11:10 -0400

    Decided to completely overhall Guess.java to build it to tests. I also created tests that all pass for Guess.java in GuessTest.java. I also created the Grader.java file. I meant to commit in stages but forgot.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Please upload assignment files here. Run `./aclean` to remove unnecessary files.
 time worked
 1hr 8-28
 2.5hrs of setting up enviroment and going through supplemental material
+1hr 8-29
---
+++ app/src/main/java/App/Guess.java
@@ -0,0 +1,80 @@
+package app;
+
+import java.util.Random;
+import java.util.Scanner;
+public class Guess {
+
+
+    public static void main(String[] args) {
+        var g = new Game(5);
+
+
+    }
+
+    static class Game {
+        int secretNum;
+        int numGuesses;
+        int guess;
+        public Game() {
+            Random rand = new Random();
+            secretNum = rand.nextInt(1, 11);
+            numGuesses = 0;
+            guess = 0;
+
+            Scanner scan = new Scanner(System.in);
+            while (guess != secretNum) {
+                System.out.println("Enter your guess: \n");
+                guess = (scan.nextInt());
+                numGuesses++;
+
+                if (guess < secretNum) {
+                    System.out.println("Your guess is too low\n");
+                } else if (guess > secretNum) {
+                    System.out.println("Your guess is too high\n");
+                } else {
+                    System.out.println("You got it!\n");
+                }
+
+
+            }
+            System.out.println("It took you " + numGuesses + " guesses \n");
+
+        }
+
+
+        public Game(int g) {
+            secretNum = g;
+            numGuesses = 0;
+            guess = 0;
+
+            while (guess != secretNum) {
+                System.out.println("Enter your guess: \n");
+                guess = g;
+                numGuesses++;
+
+                if (guess < secretNum) {
+                    System.out.println("Your guess is too low\n");
+                } else if (guess > secretNum) {
+                    System.out.println("Your guess is too high\n");
+                } else {
+                    System.out.println("You got it!\n");
+                }
+
+
+            }
+            System.out.println("It took you " + numGuesses + " guesses \n");
+
+        }
+        public String guess(int g) {
+            if (g < secretNum) return "low";
+            if (g > secretNum) return "high";
+            return "correct";
+        }
+    }
+}
+
+
+
+
+
+
